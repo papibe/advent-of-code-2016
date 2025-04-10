35f6bdbd8e3e2cdbc723cfcad32b6230e3e647a4
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-04-09 20:20:44 -0500

    Add all go files for day 6 part 2

---
+++ go/day06_part2/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+func parse(filename string) []string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic("file error")
+	}
+	return strings.Split(strings.Trim(string(data), "\n"), "\n")
+}
+
+func solve(data []string) string {
+	frequencies := []map[rune]int{}
+	for i := 0; i < len(data[0]); i++ {
+		frequencies = append(frequencies, make(map[rune]int))
+	}
+
+	for _, word := range data {
+		for index, char := range word {
+			value, ok := frequencies[index][char]
+			if !ok {
+				value = 0
+			}
+			frequencies[index][char] = value + 1
+		}
+	}
+
+	message := []string{}
+	for _, frequency := range frequencies {
+		max_freq := math.MaxInt
+		max_char := ' '
+
+		for char, freq := range frequency {
+			if freq < max_freq {
+				max_freq = freq
+				max_char = char
+			}
+		}
+		message = append(message, string(max_char))
+	}
+	return strings.Join(message, "")
+}
+
+func solution(filename string) string {
+	data := parse(filename)
+	return solve(data)
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // easter
+	fmt.Println(solution("./input.txt"))   // umejzgdw
+}
