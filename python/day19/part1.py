dcc0c5d11342375d254072d6fa6a1bcb5a301517
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-06-25 22:14:04 -0500

    Add python solution and files for day 19 part 1

---
+++ python/day19/part1.py
@@ -0,0 +1,16 @@
+def josephus(n: int) -> int:
+    if n == 1:
+        return 1
+
+    if n % 2 == 0:
+        return 2 * josephus(n // 2) - 1
+    else:
+        return 2 * josephus(n // 2) + 1
+
+
+def solution(number_of_elves: int) -> int:
+    return josephus(number_of_elves)
+
+
+if __name__ == "__main__":
+    print(solution(3014387))  # 1834471
