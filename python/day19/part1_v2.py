c9ff33757eba2d19bbbaf3bc7c3ddde8da086915
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-06-26 21:40:43 -0500

    Lint

--- python/day19/part1_v2.py
+++ python/day19/part1_v2.py
@@ -1,13 +1,14 @@
-from math import log2, floor
+from math import floor, log2
+
 
 def josephus(n: int) -> int:
-    l: int = (n - 2 ** (floor(log2(n))))
-    return 2 * l + 1
+    L: int = n - 2 ** (floor(log2(n)))
+    return 2 * L + 1
+
 
 def solution(number_of_elves: int) -> int:
     return josephus(number_of_elves)
 
 
 if __name__ == "__main__":
-    print(solution(5))  # 3
     print(solution(3014387))  # 1834471
